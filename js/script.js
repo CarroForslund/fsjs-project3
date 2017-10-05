b82972b9aba17caa2046705248aed9c9593b4fcc
Caroline Forslund <cf222ev@student.lnu.se>
2017-10-05 22:03:48 +0200

    Gets value from design select list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let otherJobRoleShown = false;
+let otherTitleShown = false;
 
 //highlight Name input field
 function highLight(){
@@ -9,35 +9,51 @@ function highLight(){
 
 //A text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
 //Give the field an id of “other-title,” and add the placeholder text of "Your Job Role" to the field.
-function jobRole(){
-  const jobRole = document.getElementById("title");
-  jobRole.onchange = function() {
-    var value = this.value;
+function titleForm(){
+  const title = document.getElementById("title");
+  title.onchange = function() {
+    const value = this.value;
     if (value !== 'other'){
-      if (otherJobRoleShown){
-        const input = document.querySelector('fieldset');
-        input.removeChild(input.lastChild);
-        otherJobRoleShown = false;
+      if (otherTitleShown){
+        const fieldset = document.querySelector('fieldset');
+        fieldset.removeChild(fieldset.lastChild);
+        otherTitleShown = false;
       };
       return;
     } else {
       const form = document.querySelector('form');
-      const basicInfo = document.querySelector('fieldset');
+      const fieldset = document.querySelector('fieldset');
       const input = document.createElement('input');
       input.type = 'text';
       input.id = "other-title";
       input.name = "user_role";
       input.placeholder = "Your Job Role";
-      basicInfo.appendChild(input);
+      fieldset.appendChild(input);
 
-      otherJobRoleShown = true;
+      otherTitleShown = true;
     };
   }
 };
 
-const runProgram = function(){
+// ”T-Shirt Info” section of the form:
+// For the T-Shirt color menu, only display the color options that
+//match the design selected in the "Design" menu.
+function shirtForm(){
+  const design = document.getElementById("design");
+  design.onchange = function() {
+    const value = this.value;
+    if(value === "js puns"){
+      console.log('js puns');
+    } else {
+      console.log('heart js');
+    };
+  };
+};
+
+function runProgram(){
   highLight();
-  jobRole();
+  titleForm();
+  shirtForm();
 };
 
 runProgram();

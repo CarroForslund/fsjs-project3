3efb8a0133d09f9401042ec9eae8038b1089a279
Caroline Forslund <cf222ev@student.lnu.se>
2017-10-05 21:26:58 +0200

    Your Job Role field removes on different option

--- js/script.js
+++ js/script.js
@@ -1,31 +1,43 @@
+let otherJobroleShown = false;
+
 //highlight Name input field
-var input = document.getElementById('name');
-input.focus();
-input.select();
+const highLight = function(){
+  var input = document.getElementById('name');
+  input.focus();
+  input.select();
+};
 
 //A text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
 //Give the field an id of “other-title,” and add the placeholder text of "Your Job Role" to the field.
 function jobRoleField(){
-  const jobRoleField = document.getElementById("title")
-  jobRoleField.onchange=function() {
+  const jobRoleField = document.getElementById("title");
+  jobRoleField.onchange = function() {
     var value = this.value;
     if (value !== 'other'){
-      console.log('nope');
+      if (otherJobroleShown){
+        const input = document.querySelector('fieldset');
+        input.removeChild(input.lastChild);
+        otherJobroleShown = false;
+      };
       return;
     } else {
-      console.log('time to print text input field to html');
       const form = document.querySelector('form');
       const basicInfo = document.querySelector('fieldset');
-      console.log(basicInfo);
       const input = document.createElement('input');
       input.type = 'text';
       input.id = "role";
       input.name = "user_role";
       input.placeholder = "Your Job Role";
       basicInfo.appendChild(input);
-      // <input type="text" id="role" name="user_role" placeholder="Your Job Role">
+
+      otherJobroleShown = true;
     };
   }
 };
 
-jobRoleField();
+const runProgram = function(){
+  highLight();
+  jobRoleField();
+};
+
+runProgram();
